b8a37269fcf1e79aaa92160a6c5fe031c73df714
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2022-10-03 18:05:42 +0200

    Initial commit

---
+++ src/Esercizi/BancaException.java
@@ -0,0 +1,15 @@
+package Esercizi;
+
+public class BancaException extends Exception{
+    private String msg;
+
+    public BancaException(String m) {
+        super(m);
+        this.msg = m;
+    }
+
+    @Override
+    public String toString() {
+        return "BANCAEXCEPTION - " + this.msg;
+    }
+}
